420ff59c323750b6f89d47ca12a27968b22f9b35
Eduardo Berton <eduardo.berton@dowjones.com>
2019-07-30 10:11:09 -0300

    initial impl of the expansion entity, plus some minor fixes

---
+++ src/main/java/magiccards/ui/entities/Expansion.java
@@ -0,0 +1,21 @@
+package magiccards.ui.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class Expansion {
+
+    private String expansionId;
+    private String name;
+    private String ptBrName;
+    private String linkName;
+    private String code;
+    private Date launchDate;
+    private Integer expansionCategoryId;
+    private Boolean isPromo;
+    private Boolean isLegal;
+}
